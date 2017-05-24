4cbc18870b8e0660839ceceb652598836daf93c0
Michael Mathers <michael.p.mathers@live.com>
2017-05-24 14:22:58 +1000

    Configuring Travis CI
    Added files to generate a project and a simple test build

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Penguin
+
+## Status
+
+### Travis-CI
+develop: [![Build Status](https://travis-ci.org/mathersm/Penguin.svg?branch=develop)](https://travis-ci.org/mathersm/Penguin)
+master: [![Build Status](https://travis-ci.org/mathersm/Penguin.svg?branch=master)](https://travis-ci.org/mathersm/Penguin)
---
+++ penguin/Exception.h
@@ -0,0 +1,25 @@
+/*
+ * (c) Copyright 2017
+ *
+ * @file     Exception.h
+ * @author   Michael Mathers
+ */
+#ifndef PENGUIN_EXCEPTION_H
+#define PENGUIN_EXCEPTION_H
+
+
+#include "Penguin_export.h"
+#include <stdexcept>
+
+
+namespace Penguin
+{
+    struct Penguin_Export TimeoutException : public std::runtime_error
+    {
+        TimeoutException(const std::string &what);
+        TimeoutException(const char *what);
+    };
+} // namespace Penguin
+
+
+#endif // PENGUIN_EXCEPTION_H
