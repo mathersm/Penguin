87911edc2886c9a9ad1e413f3d7a7f83e98d605a
Michael Mathers <michael.p.mathers@live.com>
2017-09-07 12:14:50 +0930

    Version information
    Added a version header

---
+++ penguin/Version.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 Michael Mathers
+ */
+#ifndef PENGUIN_VERSION_H
+#define PENGUIN_VERSION_H
+
+
+namespace Penguin
+{
+#define PENGUIN_VERSION "0.1.0"
+    constexpr long PENGUIN_MAJOR_VERSION = 0;
+    constexpr long PENGUIN_MINOR_VERSION = 1;
+    constexpr long PENGUIN_PATCH_VERSION = 0;
+}
+
+
+#endif // PENGUIN_VERSION_H
