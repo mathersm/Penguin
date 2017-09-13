11c53379f196c80a54bd0db9a607b07260bd5533
Michael Mathers <michael.p.mathers@live.com>
2017-09-13 10:13:42 +0930

    Version fix (#13)
    Fixed definition of PENGUIN_VERSION which seemed to be corrupted from a previous commit.
    Updated switch for C++ standard selection
    Apparently std::string_view is still experimental for a g++ build on 6.3
    Output directories were not set correctly for DLLs.
    Compile-time options seem to have to be set after project definition in CMake.
    Removing penguin.mpc.
    Add Language.h to CMakeLists.txt

--- penguin/Version.h
+++ penguin/Version.h
@@ -5,12 +5,12 @@
 #define PENGUIN_VERSION_H
 
 
-#include <string_view>
+#include "Language.h"
 
 
 namespace Penguin
 {
-    constexpr std::string_view = "0.1.0";
+    constexpr string_view PENGUIN_VERSION("0.1.0", 5);
     constexpr long PENGUIN_MAJOR_VERSION = 0;
     constexpr long PENGUIN_MINOR_VERSION = 1;
     constexpr long PENGUIN_PATCH_VERSION = 0;
