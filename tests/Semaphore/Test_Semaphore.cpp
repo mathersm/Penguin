e789ba4d2c96a931032b1946d7f18c7fcb85c074
Michael Mathers <michael.p.mathers@live.com>
2017-09-22 00:45:28 +0930

    Codacy improvements (#17)
    Added a Codacy badge to the Readme markdown
    Removed whitespace only lines.
    Ensured files end with a blank line.
    Removed capture of unused variables.
    Removed negative result checks from some tests.

--- tests/Semaphore/Test_Semaphore.cpp
+++ tests/Semaphore/Test_Semaphore.cpp
@@ -35,7 +35,7 @@ namespace
         return (result == std::cv_status::no_timeout ? 0 : -1);
     }
 
-    
+
     int try_acquire_until(Penguin::Semaphore* sema)
     {
         std::cv_status result = sema->try_acquire_until(std::chrono::system_clock::now() + std::chrono::seconds(3));
@@ -48,7 +48,7 @@ namespace
         Penguin::Semaphore semaphore(0);
 
         std::future<int> acquire_result = std::async(std::launch::async, acquire, &semaphore);
-        std::future<int> release_result = std::async(std::launch::async, release, &semaphore);
+        std::async(std::launch::async, release, &semaphore);
 
         int result = acquire_result.get();
         print_test_result(result, "test_acquire()");
@@ -58,6 +58,7 @@ namespace
 
     int test_waiters(void)
     {
+        bool successful_result = true;
         Penguin::Semaphore semaphore(0);
 
         std::vector<std::future<int>> acquire_results;
@@ -69,7 +70,7 @@ namespace
 
         size_t waiters = acquire_results.size();
 
-        int result = !(semaphore.waiters() == waiters);
+        successful_result &= (semaphore.waiters() == waiters);
         
         // Make sure we allow the other threads to come off their waits
         for (size_t i = 0; i < waiters; ++i)
@@ -78,8 +79,9 @@ namespace
         }
 
         int acquire_result = std::accumulate(acquire_results.begin(), acquire_results.end(), 0, [](int a, std::future<int>& f) {return a + f.get(); });
-        result |= acquire_result;
+        successful_result &= (acquire_result == 0);
 
+        int result = (successful_result ? 0 : -1);
         print_test_result(result, "test_waiters()");
         return result;
     }
@@ -87,9 +89,10 @@ namespace
 
     int test_permits(void)
     {
+        bool successful_result = true;
         Penguin::Semaphore semaphore(2);
 
-        int result = !(semaphore.permits() == 2);
+        successful_result &= (semaphore.permits() == 2);
 
         std::vector<std::future<int>> acquire_results;
         acquire_results.push_back(std::async(std::launch::async, acquire, &semaphore));
@@ -97,8 +100,8 @@ namespace
         acquire_results.push_back(std::async(std::launch::async, acquire, &semaphore));
 
         std::this_thread::sleep_for(std::chrono::seconds(2));
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 1);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 1);
 
         // Make sure we allow the other threads to come off their waits
         size_t waiters = semaphore.waiters();
@@ -108,8 +111,9 @@ namespace
         }
 
         int acquire_result = std::accumulate(acquire_results.begin(), acquire_results.end(), 0, [](int a, std::future<int>& f) {return a + f.get(); });
-        result |= acquire_result;
+        successful_result &= (acquire_result == 0);
 
+        int result = (successful_result ? 0 : -1);
         print_test_result(result, "test_permits()");
         return result;
     }
@@ -117,31 +121,33 @@ namespace
 
     int test_try_acquire_for(void)
     {
+        bool successful_result = true;
         Penguin::Semaphore semaphore(0);
 
-        int result = !(semaphore.permits() == 0);
+        successful_result &= (semaphore.permits() == 0);
 
         std::vector<std::future<int>> acquire_results;
         acquire_results.push_back(std::async(std::launch::async, try_acquire_for, &semaphore));
         acquire_results.push_back(std::async(std::launch::async, try_acquire_for, &semaphore));
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 2);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 2);
 
         // Release one thread
         std::async(std::launch::async, release, &semaphore);
         
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 1);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 1);
 
         // Wait for remaining threads attempting to acquire to time out
         int acquire_result = std::accumulate(acquire_results.begin(), acquire_results.end(), 0, [](int a, std::future<int>& f) {return a + f.get(); });
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 0);
-        result |= !(acquire_result == -1);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 0);
+        successful_result &= (acquire_result == -1);
 
+        int result = (successful_result ? 0 : -1);
         print_test_result(result, "test_try_acquire_for()");
         return result;
     }
@@ -149,31 +155,33 @@ namespace
 
     int test_try_acquire_until(void)
     {
+        bool successful_result = true;
         Penguin::Semaphore semaphore(0);
 
-        int result = !(semaphore.permits() == 0);
+        successful_result &= (semaphore.permits() == 0);
 
         std::vector<std::future<int>> acquire_results;
         acquire_results.push_back(std::async(std::launch::async, try_acquire_until, &semaphore));
         acquire_results.push_back(std::async(std::launch::async, try_acquire_until, &semaphore));
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 2);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 2);
 
         // Release one thread
         std::async(std::launch::async, release, &semaphore);
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 1);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 1);
 
         // Wait for remaining threads attempting to acquire to time out
         int acquire_result = std::accumulate(acquire_results.begin(), acquire_results.end(), 0, [](int a, std::future<int>& f) {return a + f.get(); });
-        result |= !(semaphore.permits() == 0);
-        result |= !(semaphore.waiters() == 0);
-        result |= !(acquire_result == -1);
+        successful_result &= (semaphore.permits() == 0);
+        successful_result &= (semaphore.waiters() == 0);
+        successful_result &= (acquire_result == -1);
 
+        int result = (successful_result ? 0 : -1);
         print_test_result(result, "test_try_acquire_until()");
         return result;
     }
