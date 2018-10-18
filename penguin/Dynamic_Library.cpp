febd2973992fcb499f2d5e8066075187ad157f21
Michael Mathers <michael.p.mathers@live.com>
2018-10-19 10:26:44 +1030

    Dynamic library loads (#23)
    * Initial implementation for performing dynamic loading of shared libraries and finding function addresses.
    * Adopted std::filesystem for working with library files and their extensions
    * Added additional library required for linking on Linux builds (libstdc++fs)

---
+++ penguin/Dynamic_Library.cpp
@@ -0,0 +1,106 @@
+/*
+* Copyright (c) 2018 Michael Mathers
+*/
+#include "Dynamic_Library.h"
+#include <iostream>
+
+
+namespace
+{
+#if defined(__GNUG__) 
+    void* get_library_function_linux(void* library_handle, const std::string& function_name)
+    {
+        // Call dlerror to clear any old error conditions
+        dlerror();
+        void* function_address = dlsym(library_handle, function_name.c_str());
+        char* error_result = dlerror();
+        if (error_result != nullptr)
+        {
+            std::cerr << "ERROR! Failed to find address for " << function_name.c_str() << " - " << error_result << '\n';
+        }
+        return function_address;
+    }
+#elif defined(_MSC_VER) 
+    FARPROC get_library_function_windows(HMODULE library_handle, const std::string& function_name)
+    {
+
+        FARPROC function_address = GetProcAddress(library_handle, function_name.c_str());
+        if (function_address == NULL)
+        {
+            std::cerr << "ERROR (" << GetLastError() << ")! - Failed to find address for " << function_name.c_str() << '\n';
+        }
+        return function_address;
+    }
+#endif
+
+
+#if defined(__GNUG__) 
+    void* load_library_linux(const std::string& library_path)
+    {
+        void* handle = dlopen(library_path.c_str(), RTLD_LAZY);
+        if (handle == NULL)
+        {
+            std::cerr << "ERROR! Failed to load library " << library_path.c_str() << " - " << dlerror() << '\n';
+        }
+        return handle;
+    }
+#elif defined(_MSC_VER) 
+    HMODULE load_library_windows(const std::string& library_path)
+    {
+        HMODULE module_handle = LoadLibrary(library_path.c_str());
+        if (module_handle == NULL)
+        {
+            std::cerr << "ERROR (" << GetLastError() << ")! - Failed to load library " << library_path.c_str() << '\n';
+        }
+        return module_handle;
+    }
+#endif
+
+
+    std::string fix_dynamic_library_filename(std::filesystem::path library_path)
+    {
+        if (library_path.has_extension() == false || library_path.extension().string().compare(Penguin::dynamic_library_extension) != 0)
+        {
+            // If there is either no extension, or an incorrect one, replace it
+            library_path.replace_extension(Penguin::dynamic_library_extension);
+        }
+#if defined(__GNUG__)
+        if (library_path.filename().string().compare(0, 3, Penguin::dynamic_library_prefix) != 0)
+        {
+            std::string new_filename(Penguin::dynamic_library_prefix);
+            new_filename.append(library_path.filename().string());
+            library_path.replace_filename(new_filename);
+        }
+#endif
+        return library_path.string();
+    }
+}
+
+
+namespace Penguin
+{
+    Library_Handle load_library(const std::filesystem::path& library_path)
+    {
+#if defined(__GNUG__) 
+        return load_library_linux(fix_dynamic_library_filename(library_path));
+#elif defined(_MSC_VER) 
+        return load_library_windows(fix_dynamic_library_filename(library_path));
+#endif
+    }
+
+
+    Function_Address get_library_function(Library_Handle library_handle, const std::string& function_name)
+    {
+#if defined(__GNUG__) 
+        return get_library_function_linux(library_handle, function_name);
+#elif defined(_MSC_VER) 
+        return get_library_function_windows(library_handle, function_name);
+#endif
+    }
+
+
+    int test_library_function(void)
+    {
+        return 1;
+    }
+}
