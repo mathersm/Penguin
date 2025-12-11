d1df581b923fc75822d6c618ff1963104e1e4185
mathersm <michael.p.mathers@live.com>
2025-12-12 01:42:47 +1030

    Swtiched to synchronised console output

--- tests/Thread_Pool/Test_Thread_Pool.cpp
+++ tests/Thread_Pool/Test_Thread_Pool.cpp
@@ -2,11 +2,16 @@
 * Copyright (c) 2025 Michael Mathers
 */
 #include <penguin/Thread_Pool.h>
+#include "penguin/Stream.h"
 #include <algorithm>
 #include <future>
 #include <iostream>
 #include <numeric>
+#include <string>
+#include <syncstream>
 #include <vector>
+
+
 
 
 namespace
@@ -14,9 +19,12 @@ namespace
     class Test_Task_Simple : public Penguin::Thread_Pool_Task
     {
     public:
-        Test_Task_Simple(void)
+        Test_Task_Simple(uint32_t task_id)
+            : Thread_Pool_Task()
+            , outStream_(std::cout)
+            , taskID_(task_id)
         {
-            // std::cout << "Test_Task_Simple::<ctor> - " << std::this_thread::get_id() << "\n";
+            Penguin::stream_out(this->outStream_, "Test_Task_Simple(", this->taskID_, ")::<ctor> - ", std::this_thread::get_id(), '\n');
         }
 
         virtual ~Test_Task_Simple(void)
@@ -26,16 +34,19 @@ namespace
 
         int run(void) override
         {
-            std::cout << "Test_Task_Simple::run - " << std::this_thread::get_id() << "\n";
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            Penguin::stream_out(this->outStream_, "Test_Task_Simple(", std::to_string(this->taskID_), ")::run - ", std::this_thread::get_id(), '\n');
             return 0;
         }
+
+    private:
+        std::osyncstream outStream_;
+        uint32_t taskID_;
     };
 
 
     void print_test_result(bool result, std::string test_text)
     {
-        std::cout << "[" << (result ? " OK " : "FAIL") << "] " << test_text.c_str() << std::endl;
+        std::osyncstream(std::cout) << "[" << (result ? " OK " : "FAIL") << "] " << test_text.c_str() << '\n';
     }
 
 
@@ -43,11 +54,14 @@ namespace
     {
         Penguin::Thread_Pool pool(4);
 
-        for (int n = 0; n < 20; n++)
+        for (uint32_t n = 0; n < 20; n++)
         {
-            std::shared_ptr<Test_Task_Simple> task = std::make_shared<Test_Task_Simple>();
+            std::shared_ptr<Test_Task_Simple> task = std::make_shared<Test_Task_Simple>(n);
             pool.execute(std::move(task));
         }
+
+        // Wait for a bit so the thread pool isn't destroyed while tasks are running, ending them prematurely
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
         return 0;
     }
@@ -57,22 +71,25 @@ namespace
     {
         Penguin::Thread_Pool pool(4);
 
-        for (int n = 0; n < 20; n++)
+        for (uint32_t n = 0; n < 20; n++)
         {
-            std::shared_ptr<Test_Task_Simple> task = std::make_shared<Test_Task_Simple>();
+            std::shared_ptr<Test_Task_Simple> task = std::make_shared<Test_Task_Simple>(n);
             pool.execute(task);
         }
 
         // This time create all the tasks up front
         std::vector<std::shared_ptr<Test_Task_Simple>> task_vector;
-        for (int n = 0; n < 50; n++)
+        for (uint32_t n = 0; n < 20; n++)
         {
-            task_vector.emplace_back(new Test_Task_Simple());
+            task_vector.emplace_back(new Test_Task_Simple(n));
         }
         for (auto& task : task_vector)
         {
             pool.execute(task);
         }
+
+        // Wait for a bit so the thread pool isn't destroyed while tasks are running, ending them prematurely
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
         return 0;
     }
@@ -113,7 +130,7 @@ namespace
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Test_Thread_Pool" << std::endl;
+    std::osyncstream(std::cout) << "Test_Thread_Pool" << '\n';
     bool pass = true;
     pass &= test_simple_execute_reference();
     pass &= test_simple_execute_move();
