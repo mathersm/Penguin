6cc84435fcb5a6ad1e4d77d26222c3f23ce9de85
mathersm <michael.p.mathers@live.com>
2025-12-11 22:28:33 +1030

    Stream helpers missed from last commit

---
+++ penguin/Stream.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Michael Mathers
+ */
+#ifndef PENGUIN_STREAM_H
+#define PENGUIN_STREAM_H
+
+
+#include <iostream>
+
+
+namespace Penguin
+{
+    template <typename STREAM, typename T>
+    void stream_out(STREAM& stream, const T& output)
+    {
+        stream << output;
+    }
+
+    template <typename STREAM, typename T, typename ...U>
+    void stream_out(STREAM& stream, const T& output, const U&... more_output)
+    {
+        stream << output;
+        return stream_out(stream, more_output...);
+    }
+}
+
+
+#endif // PENGUIN_STREAM_H
